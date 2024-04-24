9a9c5a5a6f9228c803a7364e98fbcce759a6e7e1
BekirGede <topbekir.yt@gmail.com>
2024-04-24 21:16:50 +0200

    Added Game Functions
    -Can collect cards when pair is right
    -Cards can be shuffled
    -icon list will be created out of variable

--- src/app.js
+++ src/app.js
@@ -1,59 +1,97 @@
 //References
 let cards = document.querySelectorAll(".memory-card");
-
+ 
 //Data
+let amountSelectable = 2;
 let selectedCards = [];
-let icons = ["😀","😀", "😍","😍", "😙", "😙", "😝", "😝", "😡", "😡"];
-
+let icons = ["😀", "😍", "😙", "😝", "😡", "🦇"];
+ 
+//Constants
+const fadeDelay = 1000;
+ 
 // Event Listeners
 addEventListener("click", (event) => changeCardState(event));
-
-console.log(cards[0].querySelector(".card__item").textContent);
-
-
-
-
-
-//Set Cards Randomly
-for (let i = 0; i < cards.length; i++) {
-    let usedIndex = [];
-
-    while(usedIndex.length < cards.length){
-        let rand = getRandomInt(icons.length);
-        // if (!usedIndex.includes(rand)) {        
-        //     cards[i].querySelector(".card__item").textContent = icons[rand];
-        //     usedIndex.push(rand);
-        // }
-        cards[i].querySelector(".card__item").textContent = icons[rand];
-        usedIndex.push(rand);
-    }
-}
-
-
-function changeCardState(e) {
-  if (selectedCards.length < 2) {
-    if (e.target.dataset.state === "hidden") {
-        selectedCards.push(e.target);
-        e.target.dataset.state = "open";
-
-        //If player selected two cards, hide both
-        if (selectedCards.length == 2) {
-            setTimeout(clearSelected, 1000);
+ 
+ 
+ 
+//Beginning
+createIconList();
+shuffleCards();
+ 
+ 
+ 
+function createIconList() {
+    let temp = [];
+    for (let i = 0; i < icons.length; i++) {
+        for (let j = 0; j < amountSelectable; j++) {
+            temp.push(icons[i]);
         }
     }
-  }
+    icons = temp;
 }
-
-function clearSelected() {
-    console.log(selectedCards);
-
+ 
+function shuffleCards() {
+    for (let i = icons.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = icons[i];
+        icons[i] = icons[j];
+        icons[j] = temp;
+    }
+ 
+    for (let i = 0; i < icons.length; i++) {
+        cards[i].querySelector(".card__item").textContent = icons[i];
+    }
+ 
+    console.log(icons);
+}
+ 
+function changeCardState(e) {
+    if (selectedCards.length < amountSelectable) {
+        if (e.target.dataset.state === "hidden") {
+            selectedCards.push(e.target);
+            e.target.dataset.state = "open";
+ 
+            //If player selected two cards, hide both
+            if (selectedCards.length == amountSelectable) {
+                if (!checkForMatch()) {
+                    setTimeout(() => clearSelected(true), fadeDelay);
+                }
+                else {
+                    setTimeout(collectSelected, fadeDelay);
+                }
+            }
+        }
+    }
+}
+ 
+function collectSelected() {
+    for (let i = 0; i < selectedCards.length; i++) {
+        selectedCards[i].dataset.state = "collected";
+    }
+    clearSelected(false);
+}
+ 
+function clearSelected(visualHide) {
     //Hide Cards and Clear Array
     for (let i = selectedCards.length - 1; i >= 0; i--) {
-        selectedCards[i].dataset.state = "hidden";
+        if (visualHide) { selectedCards[i].dataset.state = "hidden"; }
         selectedCards.pop();
     }
 }
-
+ 
+function checkForMatch() {
+    let won = true;
+ 
+    for (let i = 0; i < selectedCards.length - 1; i++) {
+        if (selectedCards[i].textContent !== selectedCards[i + 1].textContent) {
+            won = false;
+            break;
+        }
+    }
+ 
+    return won;
+}
+ 
 // Utility Functions
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
